e56a89957da96e96bb8b3a574c3eee9e512613d7
glauciajfr-hash <glaucia_jfr@hotmail.com>
2026-05-05 22:44:23 -0300

    feat/aplicado validação com as biblioecas class-validator e class-transformer no DTO da criação do autor

--- src/modules/autores/autores.dto.ts
+++ src/modules/autores/autores.dto.ts
@@ -1,7 +1,26 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class CriarAutorDto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
+  @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
+  @Transform(({ value }) => value.trim())
   nome: string;
+
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsNotEmpty({ message: 'O email é obrigatório' })
+  @Transform(({ value }) => value.trim())
   email: string;
 }
+
 export class AtualizarAutorDto {
   nome?: string;
   email?: string;
