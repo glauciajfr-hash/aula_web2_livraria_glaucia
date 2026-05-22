53e877a06998440cefad482aa96bebca52deecd9
glauciajfr-hash <glaucia_jfr@hotmail.com>
2026-05-21 22:32:57 -0300

    fix/ validação de autor para verificar se existe

--- src/modules/autores/autores.service.ts
+++ src/modules/autores/autores.service.ts
@@ -6,23 +6,6 @@ import {
 import { AtualizarAutorDto, CriarAutorDto } from './autores.dto';
 import { AutoresRepository } from './autores.repository';
 
-let autores = [
-  {
-    id: 1,
-    nome: 'João da Silva',
-    email: 'joao@gmail.com',
-  },
-  {
-    id: 2,
-    nome: 'Maria Oliveira',
-    email: 'maria@gmail.com',
-  },
-  {
-    id: 3,
-    nome: 'Pedro Santos',
-    email: 'pedro@gmail.com',
-  },
-];
 @Injectable()
 export class AutoresService {
   constructor(private readonly autoresRepository: AutoresRepository) {}
--- src/modules/livros/livros.service.ts
+++ src/modules/livros/livros.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { LivrosRepository } from './livros.repository';
 import { CriarLivroDto } from './livros.dto';
+import { AutoresService } from '../autores/autores.service';
 
 @Injectable()
 export class LivrosService {
-  constructor(private readonly LivrosRepository: LivrosRepository) {}
+  constructor(
+    private readonly LivrosRepository: LivrosRepository,
+    private readonly AutoresService: AutoresService,
+  ) {}
 
   async listarLivros() {
     return await this.LivrosRepository.listarLivros();
   }
 
   async criarLivro(bodyRequest: CriarLivroDto) {
+    await this.AutoresService.listarAutor(bodyRequest.id_autor);
     return await this.LivrosRepository.criarLivro(bodyRequest);
   }
 }
